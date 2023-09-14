c72152ff37f7c18ca311bd4b7a52556e319d3769
MAXIMILIANO SHER <95596083@ort>
2023-09-14 09:12:02 -0300

    14/9

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -1,3 +1,4 @@
+/*
 let nombre = prompt('Ingresa tu nombre')
 CalcularEdad(prompt('Ingresa tu fecha de nacimiento (MM/DD/AAAA)'))
 let frutaBuscada = prompt('Ingresa el nombre de la fruta')
@@ -12,7 +13,13 @@ const ciudad = {
     extensión: '1234ha'
 }
 YoObjeto(ciudad)
-
+const array1 = [1,2,3,4,5,6,7,8,9,10]
+Arrays(array1)
+EscaleraA()
+EscaleraB()
+*/
+NombresConA(prompt('Introduci los nombres separados por una coma'))
+//1
 function CalcularEdad(fecha)
 {
     const fecha1 = new Date(fecha)
@@ -26,6 +33,7 @@ function CalcularEdad(fecha)
    }
    console.log(`Hola ${nombre}, tenes ${edad} años!`)
 }
+//2
 function BuscarFruta(fruta, frutas)
 {
     const busqueda =  frutas.find((frutas) => frutas == frutaBuscada)
@@ -38,6 +46,7 @@ function BuscarFruta(fruta, frutas)
         console.log('No, no tenemos ' + frutaBuscada)   
     }
 }
+//3
 function EsIgual(){
     if(10 == '10'){
         console.log('10 es == a diez')
@@ -53,8 +62,44 @@ function EsIgual(){
         console.log('no es cierto')
     }
 }
+//4
 function YoObjeto(obj){
     for(const propiedad in obj){
         console.log(`${propiedad}: ${obj[propiedad]}`)
     }
+}
+//5
+function Arrays(array1){
+    const array2 = [];
+    for(let i = 0; i < array1.length; i++){
+        array2[i] = array1[i] * 2;
+        console.log(array2[i]);
+    }
+}
+//6A
+function EscaleraA(){
+    let sts = '';
+    for(let i = 0; i<5; i++){
+        sts += '*'
+        console.log(sts)
+    }
+}
+//6B
+function EscaleraB(){
+    let sts = '*';
+    let fondo = '----'
+    for(let i = 0; i<5; i++){
+        console.log(`${fondo}${sts}${fondo}`)
+        fondo = fondo.slice(1)
+        sts +='**'
+    }
+}
+//7
+function NombresConA(str){
+    let array = str.split(',')
+    array.forEach(element => {
+        if(element[0] != 'A' && element[0] != 'a'){
+
+        }
+    });
 }
